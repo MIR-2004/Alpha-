b8b0a308ffaca3695883cd08604e582078ed3ad8
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2025-08-22 01:45:40 +0530

    new

--- 32.Binary_Search_Tree(Part-1)/BuildBST.java
+++ 32.Binary_Search_Tree(Part-1)/BuildBST.java
@@ -1,35 +1,39 @@
-class BuildBST{
 
-    static class Node{
+import java.util.ArrayList;
+
+
+
+class BuildBST {
+
+    static class Node {
         int data;
         Node left;
         Node right;
-        
-        Node(int data){
+
+        Node(int data) {
             this.data = data;
         }
     }
 
+    public static Node insert(int data, Node root) {
+        if (root == null) {
+            root = new Node(data);
+            System.out.println("Inserted " + data + "in the BST.");
+        }
 
-    public static Node insert(int data, Node root){
-       if(root == null){
-        root = new Node(data);
-        System.out.println("Inserted "+data+"in the BST.");
-       }
-
-       if(root.data > data){
+        if (root.data > data) {
             root.left = insert(data, root.left);
-       }
+        }
 
-       if(root.data < data){
-        root.right = insert(data, root.right);
-       }
+        if (root.data < data) {
+            root.right = insert(data, root.right);
+        }
 
-       return root;
+        return root;
     }
 
-    public static void inorder(Node root){
-        if(root == null){
+    public static void inorder(Node root) {
+        if (root == null) {
             return;
         }
 
@@ -38,27 +42,131 @@ class BuildBST{
         inorder(root.right);
     }
 
-    public static boolean search(Node root, int data){
-        if(root == null){
+    public static boolean search(Node root, int data) {
+        if (root == null) {
             return false;
         }
-        if(root.data == data){
+        if (root.data == data) {
             return true;
         }
-        if(data < root.data){
+        if (data < root.data) {
             return search(root.left, data);
-        }else{
+        } else {
             return search(root.right, data);
         }
     }
-    public static void main(String[] args){
-        int arr[] = {5,1,3,2,4,7};
-        Node n = new Node(5);
+
+    public static Node delete(Node root, int val) {
+        if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else if (root.data < val) {
+            root.right = delete(root.right, val);
+        } else {
+
+            // 0 leaf nodes
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            // one leaf node is null
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // no leaf node is null
+
+            Node IS = inordersucceser(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+
+        }
+
+        return root;
+    }
+
+    public static Node inordersucceser(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static void printInRange(Node root, int num1, int num2){
+        if(root == null){
+            return;
+        }
+        if(root.data <= num2 && root.data >= num1){
+            printInRange(root.left, num1, num2);
+            System.out.print(root.data+" ");
+            printInRange(root.right, num1, num2);
+        }else if(root.data > num2){
+            printInRange(root.left, num1, num2);
+        }else{
+            printInRange(root.right, num1, num2);
+        }
+
+    }
+
+    public static void pathFinder(Node root, ArrayList<Number> nums){
+
+        if(root == null){
+            return;
+        }
+
+        nums.add(root.data);
+
+        if(root.left == null && root.right == null){
+            for(int i = 0; i < nums.size(); i++){
+                System.out.print(nums.get(i) + " ");
+            }
+            System.out.println();
+        }
+        pathFinder(root.left, nums);
+        pathFinder(root.right, nums);
+        nums.removeLast();
+    }
+
+    public static boolean isValid(Node root, Node max, Node min){
+        if(root == null){
+            return true;
+        }
+
+        if(min != null && root.data <= min.data){
+            return false;
+        }
+
+        if(max != null && root.data >= max.data){
+            return false;
+        }
+
+        return isValid(root.left, max, min) && isValid(root.right, max, min);
+    }
+
+    public static Node mirror(Node root){
+        if(root == null){
+            return null;
+        }
+
+        Node left = mirror(root.left);
+        Node right = mirror(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
+        Node n = new Node(8);
+        ArrayList<Number> nums = new ArrayList<>();
 
         for (int i = 1; i < arr.length; i++) {
             insert(arr[i], n);
         }
 
-        System.out.println(search(n, 15));
     }
 }

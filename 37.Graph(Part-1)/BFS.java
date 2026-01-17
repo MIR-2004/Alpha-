933962ea2e23653d49694cbecf63a3b446c97aca
MIR-2004 <mir.saif.ali2004@gmail.com>
2026-01-17 12:58:25 +0530

    new

--- 37.Graph(Part-1)/BFS.java
+++ 37.Graph(Part-1)/BFS.java
@@ -1,45 +1,68 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.util.LinkedList;
 
-public class BFS {
-    
-    public static class Edge{
+class BFS {
+
+    public static class Edge {
+
         int src;
         int dest;
-        int weight;
 
-        public Edge(int s, int d, int w){
+        public Edge(int s, int d) {
             this.src = s;
             this.dest = d;
-            this.weight = w; 
         }
-
     }
-    public static void main(String args[]){
-        int v = 5;
-        ArrayList<Edge>[] graph = new ArrayList[v]; 
 
-        for(int i = 0; i < v; i++){
+    public static void main(String arg[]) {
+        int v = 7;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+
+        for (int i = 0; i < v; i++) {
             graph[i] = new ArrayList<>();
         }
 
-        //vertex 0
-        graph[0].add(new Edge(0, 1, 5));
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
 
-        //vertex 1
-        graph[1].add(new Edge(1, 0, 5));
-        graph[1].add(new Edge(1, 2, 1));
-        graph[1].add(new Edge(1, 3, 3));
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
 
-        //vertex 2
-        graph[2].add(new Edge(2, 1, 1));
-        graph[2].add(new Edge(2, 3, 1));
-        graph[2].add(new Edge(2, 4, 2));
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
 
-        //vertex 3
-        graph[3].add(new Edge(3, 1, 3));
-        graph[3].add(new Edge(3, 2, 1));
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
 
-        //vertex 4
-        graph[4].add(new Edge(4, 2, 2));
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+
+        graph[6].add(new Edge(6, 5));
+
+        //BFS
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[v];
+
+        q.add(0);
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+
+            if (!vis[curr]) {
+                System.out.print(curr + " ");
+                vis[curr] = true;
+
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    q.add(graph[curr].get(i).dest);
+                }
+            }
+        }
+
     }
 }
